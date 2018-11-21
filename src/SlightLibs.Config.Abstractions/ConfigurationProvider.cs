36dde99b99a02b6e9377211dc5593c4d629ea2d4
johmarjac <johannesmjacobs@gmail.com>
2018-11-21 12:17:15 +0100

    Configuration Abstraction Layer + Json Implementation

---
+++ src/SlightLibs.Config.Abstractions/ConfigurationProvider.cs
@@ -0,0 +1,26 @@
+﻿using SlightLibs.Service;
+using System;
+
+namespace SlightLibs.Config.Abstractions
+{
+    [InjectableService]
+    public abstract class ConfigurationProvider : IConfigurationProvider
+    {
+        public abstract void Load();
+
+        public abstract void Save();
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if(disposing)
+            {
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ src/SlightLibs.Config.Json/JsonConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using SlightLibs.Config.Abstractions;
+using System.IO;
+
+namespace SlightLibs.Config.Json
+{
+    public abstract class JsonConfiguration : ConfigurationProvider
+    {
+        [JsonIgnore]
+        protected string ConfigFile { get; set; } = "config.json";
+
+        public sealed override void Load()
+        {
+            var fInfo = new FileInfo(ConfigFile);
+            if (!fInfo.Exists)
+                Save();
+
+            JsonConvert.PopulateObject(File.ReadAllText(ConfigFile), this);
+        }
+
+        public sealed override void Save()
+        {
+            File.WriteAllText(ConfigFile, JsonConvert.SerializeObject(this, Formatting.Indented));
+        }
+    }
+}
