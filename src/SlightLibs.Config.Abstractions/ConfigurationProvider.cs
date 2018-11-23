71027d3540cc55fb6f621c601b2a251ff333e303
johmarjac <johannesmjacobs@gmail.com>
2018-11-23 14:48:42 +0100

    Documentation

--- src/SlightLibs.Config.Abstractions/ConfigurationProvider.cs
+++ src/SlightLibs.Config.Abstractions/ConfigurationProvider.cs
@@ -6,8 +6,14 @@ namespace SlightLibs.Config.Abstractions
     [InjectableService]
     public abstract class ConfigurationProvider : IConfigurationProvider
     {
+        /// <summary>
+        /// Loads the data
+        /// </summary>
         public abstract void Load();
 
+        /// <summary>
+        /// Saves the data
+        /// </summary>
         public abstract void Save();
 
         protected virtual void Dispose(bool disposing)
